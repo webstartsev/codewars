962cecb7ea305f056f379a7a72cba2d5fcd10cdb
Sergey Startsev <sergey.starcev@imeretinka.ru>
2019-08-26 17:52:43 +0300

    ОпередÐrewirite on sort, but very slow

--- 5kyu/scramble.js
+++ 5kyu/scramble.js
@@ -1,26 +1,16 @@
 // https://www.codewars.com/kata/55c04b4cc56a697bb0000048/train/javascript
 
 function scramble(str1, str2) {
-  if (str2.length > str1.length) {
-    return false;
-  }
-  if (str1 === str2) {
-    return true;
-  }
-
-  if (str1.length === str2.length && str1 !== str2) {
-    return false;
-  }
-  //code me
   let arr1 = str1.split('').sort();
   let arr2 = str2.split('').sort();
-  for (let i = 0; i < arr2.length; i++) {
-    let pos = arr1.indexOf(arr2[i]);
-    if (pos !== -1) {
-      arr1.splice(pos, 1);
-    } else {
-      return false;
+  
+  let x = 0;
+  
+  for (let i = 0; i < arr1.length; i++) {
+    if(arr1[i] === arr2[x]){
+      x++;
     }
   }
-  return true;
+  
+  return x === arr2.length;
 }
