12e135e46e758dd9d01fd8a9b490d17e3fae4790
Sergey Startsev <sergey.starcev@imeretinka.ru>
2019-08-29 16:34:38 +0300

    add test

---
+++ 4kyu/Strip Comments/index.test.js
@@ -0,0 +1,15 @@
+function checkComments(input, markers, expected) {
+  var actual;
+  actual = solution(input, markers);
+  return Test.expect(
+    actual === expected,
+    "Returned '" + actual + "' but expected '" + expected + "'"
+  );
+}
+
+checkComments(
+  'apples, plums % and bananas\npears\noranges !applesauce',
+  ['%', '!'],
+  'apples, plums\npears\noranges'
+);
+checkComments('Q @b\nu\ne -e f g', ['@', '-'], 'Q\nu\ne');
